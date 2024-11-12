4a3c1aa9e20e193809e4f0f21bf8fdd565d9b3f0
马珂 <Ma3444055841@outlook.com>
2024-11-12 13:58:07 +0800

    F[ENHANCEMENT] request-js-encapsulation-cooperation-and-backend-linking

--- README.md
+++ README.md
@@ -2,4 +2,108 @@
 
 ## 说明
 
-本仓库是微信小程序客户端的上游仓库，马恒超作为Maintainer。
+​	本仓库是微信小程序客户端的上游仓库，马恒超作为Maintainer。
+
+## 项目启动说明
+
+​	本项目也使用了npm进行附件包的下载，需要在小程序内进行项目配置。
+
+### project.config.json
+
+​	在本地需要进行`project.config.json`的修改，最重要的是修改`packNpmManually`和`packNpmRelationList`的内容，以下是我本地`project.config.json`的内容
+
+```json
+{
+  "compileType": "miniprogram",
+  "libVersion": "trial",
+  "packOptions": {
+    "ignore": [],
+    "include": []
+  },
+  "setting": {
+    "coverView": true,
+    "es6": true,
+    "postcss": true,
+    "minified": true,
+    "enhance": true,
+    "showShadowRootInWxmlPanel": true,
+    "packNpmManually": true,
+    "packNpmRelationList": [
+      {
+        "packageJsonPath": "./package.json",
+        "miniprogramNpmDistDir": "./"
+      }
+    ],
+    "babelSetting": {
+      "ignore": [],
+      "disablePlugins": [],
+      "outputPath": ""
+    }
+  },
+  "condition": {},
+  "editorSetting": {
+    "tabIndent": "auto",
+    "tabSize": 2
+  },
+  "appid": "这里是你自己的appid"
+}
+```
+
+###  npm  install
+
+```cmd
+npm install
+```
+
+​	第一次使用Npm包的时候，都需要先npm install将package.json的内容，先下载到本地中。
+
+### 构建 npm 包
+
+​	打开微信开发者工具，点击 工具 -> 构建 npm，并勾选 使用 npm 模块 选项，可见官方文档 快速上手 的 步骤四。新版的微信开发者工具中，详情 -> 本地设置中没有【使用 npm 模块】选项，则不用理会, 如果有则需要勾选。
+
+### 关闭http检查
+
+​	打开微信开发者工具，点击右上角 详情 ->本地设置 -> 不校验合法域名、web-view、tls版本以及HTTP证书，将该选项✔
+
+​	后续的步骤和原先一样，没什么区别
+
+## 项目前端进度
+
+```
+根目录
+├─.gitlab
+│  ├─issue_templates
+│  └─merge_request_templates
+├─asserts
+│  └─images
+│      ├─index
+│      ├─order
+│      ├─profile
+│      └─register
+├─components
+│  ├─plugins
+│  └─templates
+├─config
+├─miniprogram_npm
+│  └─@vant
+│      └─weapp
+├─node_modules
+│  └─@vant
+│      └─weapp
+├─pages
+│  ├─addAddress
+│  ├─complaintFeedback
+│  ├─cooperation
+│  ├─index
+│  ├─manageAddress
+│  ├─modifyInformation
+│  ├─order
+│  ├─order_info
+│  ├─profile
+│  └─register
+├─request
+└─utils
+```
+
+
+
--- pages/cooperation/cooperation.js
+++ pages/cooperation/cooperation.js
@@ -1,77 +1,100 @@
+// 引入vant提示插件
+import Toast from '@vant/weapp/toast/toast'
+// 引入统一管理的接口js
+const api = require("../../request/api")
+// 引入全局对象
+const app = getApp()
 Page({
     data: {
-      name: '',
-      phone: '',
-      region: '',
-      company: '',
-      intent: ''
-    },
-  
-    onInputName(e) {
-      this.setData({
-        name: e.detail.value
-      });
-    },
-  
-    onInputPhone(e) {
-      this.setData({
-        phone: e.detail.value
-      });
-    },
-  
-    onInputRegion(e) {
-      this.setData({
-        region: e.detail.value
-      });
-    },
-  
-    onInputCompany(e) {
-      this.setData({
-        company: e.detail.value
-      });
-    },
-  
-    onInputIntent(e) {
-      this.setData({
-        intent: e.detail.value
-      });
-    },
-  
-    submitCooperation() {
-      const { name, phone, region } = this.data;
-  
-      if (!name || !phone || !region) {
-        wx.showToast({
-          title: '姓名、联系方式和期望合作区域为必填项',
-          icon: 'none'
-        });
-        return;
-      }
-  
-      // 验证手机号码格式
-      const phoneRegex = /^1[3-9]\d{9}$/;
-      if (!phoneRegex.test(phone)) {
-        wx.showToast({
-          title: '请输入有效的手机号码',
-          icon: 'none'
-        });
-        return;
-      }
-  
-      // 提交逻辑，例如发请求等
-      wx.showToast({
-        title: '提交成功',
-        icon: 'success'
-      });
-  
-      // 清空表单
-      this.setData({
         name: '',
         phone: '',
         region: '',
         company: '',
-        intent: ''
-      });
+        intent: '',
+    },
+
+    onInputName(e) {
+        this.setData({
+            name: e.detail.value
+        });
+    },
+
+    onInputPhone(e) {
+        this.setData({
+            phone: e.detail.value
+        });
+    },
+
+    onInputRegion(e) {
+        this.setData({
+            region: e.detail.value
+        });
+    },
+
+    onInputCompany(e) {
+        this.setData({
+            company: e.detail.value
+        });
+    },
+
+    onInputIntent(e) {
+        this.setData({
+            intent: e.detail.value
+        });
+    },
+
+    submitCooperation() {
+        const {
+            name,
+            phone,
+            region
+        } = this.data;
+
+        if (!name || !phone || !region) {
+            wx.showToast({
+                title: '姓名、联系方式和期望合作区域为必填项',
+                icon: 'none'
+            });
+            return;
+        }
+
+        // 验证手机号码格式
+        const phoneRegex = /^1[3-9]\d{9}$/;
+        if (!phoneRegex.test(phone)) {
+            wx.showToast({
+                title: '请输入有效的手机号码',
+                icon: 'none'
+            });
+            return;
+        }
+
+        const submissionData = {
+            license:null,
+            email:"Ma3444055841@outlook.com",
+            phone_number:this.data.phone,
+            description:"asagfbfbaefbfgbasvcas",
+            name:this.data.name
+        }
+
+        api.cooperation(submissionData)
+            .then(res => {
+                // 提交逻辑，例如发请求等
+                wx.showToast({
+                    title: '提交成功',
+                    icon: 'success'
+                });
+                // 清空表单
+                this.setData({
+                    name: '',
+                    phone: '',
+                    region: '',
+                    company: '',
+                    intent: ''
+                });
+                console.log(res);
+            })
+            .catch(err => {
+                Toast("提交失败")
+            })
     }
-  });
-  
+});
